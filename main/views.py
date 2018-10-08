ab3a692acb39e58cce5eec15e7b8c85f323b0f1a
meduzik <aashustrov@gmail.com>
2018-10-08 10:21:44 +0300

    add view logic  and more

--- main/views.py
+++ main/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import DetailView, ListView
 
 # Create your views here.
+
+
+class Index(View):
+
+    def get(self, request):
+
+        return render(request, 'index.html')
+
+
+class About(View):
+
+    def get(self, request):
+
+        return render(request, 'about.html')
+
+
+class Contact(View):
+
+    def get(self, request):
+
+        return render(request, 'contact.html')
+
+
+class Post(View):
+
+    def get(self, request):
+
+        return render(request, 'post.html')
