56380b4f507d119866bb4d9abcaa036021aec4e3
meduzik <aashustrov@gmail.com>
2018-12-17 15:35:07 +0300

    deleted password and created dev_settings.py

---
+++ blog/dev_settings.py
@@ -0,0 +1,4 @@
+EMAIL_HOST_USER = 'kintawer@gmail.com'
+DEFAULT_FROM_EMAIL = 'kintawer@gmail.com'
+SERVER_EMAIL = 'kintawer@gmail.com'
+EMAIL_HOST_PASSWORD = '2VqzB1At'
--- blog/settings.py
+++ blog/settings.py
@@ -140,11 +140,16 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'kintawer@gmail.com'
-DEFAULT_FROM_EMAIL = 'kintawer@gmail.com'
-SERVER_EMAIL = 'kintawer@gmail.com'
-# todo: enter password!
-EMAIL_HOST_PASSWORD = '2VqzB1At'
+EMAIL_HOST_USER = ''
+DEFAULT_FROM_EMAIL = ''
+SERVER_EMAIL = ''
+EMAIL_HOST_PASSWORD = ''
+
+# getting email and password from dev_settings.py
+try:
+    from .dev_settings import *
+except ImportError:
+    pass
 
 # REDIS related settings
 REDIS_HOST = '127.0.0.1'
